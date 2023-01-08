af44128df96555948ce3fade029c9e47b2897908
Juan Tuñon <primerproyecto@gmail.com>
2023-01-08 12:53:27 +0100

    ajustes para movil

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import { Logout } from '../utils/utils';
 const template = () => `
 
 <nav>
-    <a class="efectoEnlace" href="/" data-link><h1><i data-feather="home"></i><span>Hub de games</span></h1><span aria-hidden="true"><h1><i data-feather="home"></i><span>Hub de games</span></h1></span></a>
-    <a class="efectoEnlace" href="#" onclick="${SwitchTheme()}"><span><i data-feather="refresh-ccw"></i>Refrescar color</span><span aria-hidden="true"><i data-feather="refresh-ccw"></i>Refrescar color</span></a><a class="efectoEnlace" href="/" id="logouBtn"><span><i data-feather="log-out"></i>Logout</span><span aria-hidden="true"><i data-feather="log-out"></i>Logout</span></a>
+    <a class="efectoEnlace" href="/" data-link><h1><i data-feather="home"></i><span class="ocultarEnMovil">Hub de games</span></h1><span aria-hidden="true"><h1><i data-feather="home"></i><span class="ocultarEnMovil">Hub de games</span></h1></span></a>
+
+    <a class="efectoEnlace" href="#" onclick="${SwitchTheme()}"><span><i data-feather="refresh-ccw"></i><span class="ocultarEnMovil">Refrescar color</span><span aria-hidden="true"><i data-feather="refresh-ccw"></i><span class="ocultarEnMovil">Refrescar color</span></a>
+    
+    <a class="efectoEnlace" href="/" id="logouBtn"><span><i data-feather="log-out"></i><span class="ocultarEnMovil">Logout</span><span aria-hidden="true"><i data-feather="log-out"></i><span class="ocultarEnMovil">Logout</span></a>
 </nav>
 `;
 
